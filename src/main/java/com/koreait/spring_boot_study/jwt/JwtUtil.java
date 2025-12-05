2fb4dcc036c0e9e89a00af1324aaf5dfdea5e4ea
elikese <elikese@gmail.com>
2025-12-05 18:56:34 +0900

    feat: secret

--- src/main/java/com/koreait/spring_boot_study/jwt/JwtUtil.java
+++ src/main/java/com/koreait/spring_boot_study/jwt/JwtUtil.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-import java.util.Base64;
 import java.util.Map;
 
 @Component
